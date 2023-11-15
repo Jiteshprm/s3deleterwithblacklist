65c72cc2c48dece3738ed2a5b47679376308a500
Jiteshprm <jiteshprm@gmail.com>
2023-11-15 13:16:24 +0000

    Update read_parquet_footer.py

--- read_parquet_footer.py
+++ read_parquet_footer.py
@@ -1,45 +1,49 @@
 import boto3
-from botocore.exceptions import NoCredentialsError
+import io
 
-def read_last_chars_from_s3(bucket_name, object_key, num_chars):
+def read_last_chars_from_s3_binary_file(bucket_name, key, num_chars=1000):
     s3 = boto3.client('s3')
 
-    try:
-        # Get the object from S3
-        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+    # Get the object's metadata to determine its size
+    response = s3.head_object(Bucket=bucket_name, Key=key)
+    content_length = response['ContentLength']
 
-        # Read the last 'num_chars' characters
-        content = response['Body'].read().decode('utf-8')
-        last_chars = content[-num_chars:]
+    # Calculate the range for the last 'num_chars' bytes
+    start_byte = max(0, content_length - num_chars)
+    range_header = f"bytes={start_byte}-{content_length-1}"
 
-        return last_chars
-    except NoCredentialsError:
-        print("Credentials not available.")
-    except Exception as e:
-        print(f"An error occurred while reading from S3: {e}")
-    return None
+    # Request the specified range of bytes from S3
+    response = s3.get_object(Bucket=bucket_name, Key=key, Range=range_header)
+    content = response['Body'].read()
 
-def search_strings_in_content(content):
-    # Check if the strings "parquet-" and "cdh" exist
-    if "parquet-" in content and "cdh" in content:
-        print("Both strings 'parquet-' and 'cdh' exist in the last 1000 characters.")
-    elif "parquet-" in content:
-        print("String 'parquet-' exists in the last 1000 characters.")
-    elif "cdh" in content:
-        print("String 'cdh' exists in the last 1000 characters.")
-    else:
-        print("Neither 'parquet-' nor 'cdh' found in the last 1000 characters.")
+    return content
 
-# Replace these variables with your AWS S3 details
-aws_access_key = 'your_access_key'
-aws_secret_key = 'your_secret_key'
+def search_and_print_patterns(data):
+    parquet_pattern = b'parquet-'
+    cdh_pattern = b'cdh'
+
+    parquet_start = data.find(parquet_pattern)
+
+    if parquet_start != -1:
+        # Find the next occurrence of the null character (0) after parquet-
+        null_char_index = data.find(b'\x00', parquet_start)
+        if null_char_index != -1:
+            parquet_end = null_char_index
+            parquet_data = data[parquet_start:parquet_end].decode('utf-8')
+            print(f"Found parquet- pattern: {parquet_data}")
+        else:
+            print("parquet- pattern found, but no null character (0) after it.")
+    
+    cdh_start = data.find(cdh_pattern)
+    if cdh_start != -1:
+        print("Found cdh pattern.")
+
+# Replace 'your_bucket_name' and 'your_file_key' with the actual S3 bucket name and file key
 bucket_name = 'your_bucket_name'
-object_key = 'your_object_key'
-num_chars = 1000
+file_key = 'your_file_key'
 
-# Read the last 1000 characters directly from S3
-content = read_last_chars_from_s3(bucket_name, object_key, num_chars)
+# Read the last 1000 characters from the binary file in S3
+binary_data = read_last_chars_from_s3_binary_file(bucket_name, file_key)
 
-# Search for strings in the content
-if content is not None:
-    search_strings_in_content(content)
+# Search for patterns and print results
+search_and_print_patterns(binary_data)
